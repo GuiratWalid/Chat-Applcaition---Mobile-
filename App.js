794721ce72696c56fa880659d5acb82acb62a9fe
unknown <guiratwalid98@gmail.com>
2022-11-25 19:41:43 +0100

    Change profile picture

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
 import Auth from "./screens/Auth";
 import Home from "./screens/Home";
 import Signup from "./screens/Signup";
-import { API_KEY, AUTH_DOMAIN } from "@env";
 
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  console.log(API_KEY);
   return (
-    <h1>Bonjour</h1>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen
-    //       name="auth"
-    //       component={Auth}
-    //       options={{ headerShown: false }}
-    //     ></Stack.Screen>
-    //     <Stack.Screen
-    //       name="signup"
-    //       component={Signup}
-    //       options={{ headerShown: false }}
-    //     ></Stack.Screen>
-    //     <Stack.Screen
-    //       name="home"
-    //       component={Home}
-    //       options={{ headerShown: false }}
-    //     ></Stack.Screen>
-    //     {/* <Stack.Screen name="Chat" component={Chat}></Stack.Screen> */}
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="auth"
+          component={Auth}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="signup"
+          component={Signup}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="home"
+          component={Home}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        {/* <Stack.Screen name="Chat" component={Chat}></Stack.Screen> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
